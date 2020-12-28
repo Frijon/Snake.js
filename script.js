37490bca27aa165151ef70e14aa10ad1554a5454
Orios <barinthe1st@gmail.com>
2020-12-28 16:57:48 +0100

    added border tggl+startstop+restart function+food

--- script.js
+++ script.js
@@ -1,33 +1,74 @@
 let snakebit;
 let snake = [];
 let frame;
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 500;
+let foodbit;
+let food = [];
+let borderTp = false;
+let score;
 
 function setup() {
-  createCanvas(GAME_WIDTH, GAME_HEIGHT);
-  snakebit = new Snake();
-  snake.unshift(snakebit);
-  frame = 0;
+  createCanvas(500, 500);
+  restart();
 }
 
 function draw() {
   background(0);
   if (frame % 10 == 0) {
-    snake[0].update();
 
-    for (let i = 1; i < snake.length; i++) {
-      snake[i].posx = snake[i - 1].posx;
-      snake[i].posy = snake[i - 1].posy;
+    if (snake[0].dir != "") {
+      for (let i = snake.length - 1; i >= 1; i--) {
+        snake[i].posX = snake[i - 1].posX;
+        snake[i].posY = snake[i - 1].posY;
+      }
     }
+    snake[0].update();
   }
 
-  for (let i = 0; i < snake.length; i++) {
-    snake[i].draw(i);
-  }
+  for (let i = 0; i < snake.length; i++) snake[i].draw(i);
+  for (let i = 0; i < food.length; i++) food[i].draw();
 
   frame++;
   frame %= 100;
+
+  document.getElementById("score").innerHTML = "Score: " + score;
+
+  if (snake.length >= (width * height)/10) alert("You won! Wow, congrats on that achievement.")
 }
 
-//new snakebits get spawned on top of 1st one
+
+function restart() {
+  snake = [];
+  food = [];
+
+  for (let i = 4; i > 1; i--) {
+    snake.push(new Snake(i * 10, 10));
+    food.push(new Food());
+  }
+
+  frame = 0;
+  score = 0;
+}
+
+function toggleBorders() {
+  if (confirm("do you want to restart?")) {
+    switch (borderTp) {
+      case true:
+        borderTp = false;
+        break;
+      case false:
+        borderTp = true;
+        break;
+    }
+    restart(); 
+  }
+}
+
+function toggleLoop(){
+  if (isLooping()){
+    document.getElementById("startStopButton").value="Resume";
+    noLoop();
+  } else {
+    document.getElementById("startStopButton").value="Stop";
+    loop();
+  }
+}
