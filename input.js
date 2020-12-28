7171d506a25b1b89512385226cc685fbf12f7b47
Orios <barinthe1st@gmail.com>
2020-12-28 16:54:23 +0100

    improved movement

--- input.js
+++ input.js
@@ -1,18 +1,18 @@
 function keyPressed(){
-    if (keyCode === UP_ARROW){
-        snake[0].nextdir = "up";
+    if (keyCode === UP_ARROW && snake[0].dir != "down"){
+        snake[0].nextDir = "up";
     }
 
-    if (keyCode === DOWN_ARROW){
-        snake[0].nextdir = "down";
+    if (keyCode === DOWN_ARROW && snake[0].dir != "up"){
+        snake[0].nextDir = "down";
     }
 
-    if (keyCode === RIGHT_ARROW){
-        snake[0].nextdir = "right";
+    if (keyCode === RIGHT_ARROW && snake[0].dir != "left"){
+        snake[0].nextDir = "right";
     }
 
-    if (keyCode === LEFT_ARROW){
-        snake[0].nextdir = "left";
+    if (keyCode === LEFT_ARROW && snake[0].dir != "right"){
+        snake[0].nextDir = "left";
     }
 
     if (keyCode == 71){
